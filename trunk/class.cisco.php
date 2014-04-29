3c567ed5c6286c4c762141cbce52c50942573994
detain <detain@85d27172-5045-410c-99a1-53c0f0f47f7f>
2014-04-29 21:32:08 +0000

    sorta  a dumb fix ..   in converting the old parser functiosn to classes i put in some extra code that should have been a seperate script.. fixed

--- trunk/class.cisco.php
+++ trunk/class.cisco.php
@@ -687,7 +687,7 @@
 	
 	class cisco_pasrser
 	{
-			function get_space_depth($lines, $x)
+		function get_space_depth($lines, $x)
 		{
 			if (preg_match('/^(?P<spaces>\s+)*(?P<rest>\S.*)$/', $lines[$x], $matches))
 			{
@@ -741,24 +741,6 @@
 			}
 			return $data;
 		}
-		if (!isset($_SERVER['argv'][1]) || !file_exists($_SERVER['argv'][1]))
-		{
-			die('Specify a (valid) file as the first argument to get it parsed');
-		}
-		$file = str_replace("\r", "", file_get_contents($_SERVER['argv'][1]));
-		$lines = explode("\n", $file);
-		$start_str = 'Building configuration...';
-		$x = 0;
-		while (substr($lines[$x], 0, strlen($start_str)) != $start_str)
-			$x++;
-		$info = array();
-		if (preg_match('/^Current configuration\s*:\s*(?P<config_bytes>$|\d+)( bytes)$/', $lines[$x + 2], $matches))
-		{
-			$info['config_bytes'] = $matches['config_bytes'];
-		}
-		$x += 3;
-		$info['data'] = parse_cisco_children($lines, $x + 1);
-		print_r($info);
 	}
 		
 ?>
