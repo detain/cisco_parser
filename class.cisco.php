4aefe7ef6762d44b1fc3eb4f653f1ba4fc07052b
Joe Huss <detain@interserver.net>
2017-08-06 11:41:12 -0400

    lots of update and improvements

--- class.cisco.php
+++ class.cisco.php
@@ -798,7 +798,7 @@ class cisco_parser {
 		$data = [];
 		$last_command = FALSE;
 		for ($xMax = count($lines); $x < $xMax; $x++) {
-			$cdepth = get_space_depth($lines, $x);
+			$cdepth = $this->get_space_depth($lines, $x);
 			$command = ltrim($lines[$x]);
 			$arguments = '';
 			$spacepos = mb_strpos($command, ' ');
@@ -812,10 +812,10 @@ class cisco_parser {
 				if ($arguments != '')
 					$new_data['arguments'] = trim($arguments);
 				if ($x + 1 < count($lines)) {
-					$next_depth = get_space_depth($lines, $x + 1);
+					$next_depth = $this->get_space_depth($lines, $x + 1);
 					if ($next_depth > $depth) {
-						$new_data['children'] = parse_cisco_children($lines, $x + 1, $next_depth);
-						while (get_space_depth($lines, $x + 1) > $depth)
+						$new_data['children'] = $this->parse_cisco_children($lines, $x + 1, $next_depth);
+						while ($this->get_space_depth($lines, $x + 1) > $depth)
 							++$x;
 					}
 				}
