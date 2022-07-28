abe4e41f78e8534981562b812eb3b2ab8e974ba3
Joe Huss <detain@interserver.net>
2022-07-28 17:04:07 -0400

    formatting update

--- cisco_parser.php
+++ cisco_parser.php
@@ -1,19 +1,20 @@
 <?php
+
 /* This was probably written before i wrote the class, so need to update a few links to use the class instead */
 include 'class.cisco.php';
 if (!isset($_SERVER['argv'][1]) || !file_exists($_SERVER['argv'][1])) {
-	die('Specify a (valid) file as the first argument to get it parsed');
+    die('Specify a (valid) file as the first argument to get it parsed');
 }
 $file = str_replace("\r", '', file_get_contents($_SERVER['argv'][1]));
 $lines = explode("\n", $file);
 $start_str = 'Building configuration...';
 $x = 0;
 while (mb_substr($lines[$x], 0, mb_strlen($start_str)) != $start_str) {
-	$x++;
+    $x++;
 }
 $info = [];
 if (preg_match('/^Current configuration\s*:\s*(?P<config_bytes>$|\d+)( bytes)$/', $lines[$x + 2], $matches)) {
-	$info['config_bytes'] = $matches['config_bytes'];
+    $info['config_bytes'] = $matches['config_bytes'];
 }
 $x += 3;
 $cisco = new cisco();
